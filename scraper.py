6a93b660f3ead02b3838996da29b855691b281d2
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-01 14:47:43 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,12 +17,13 @@ for row in reader:
     record['Basic'] = row[1]
     record['SRA'] = row[2]
     record['Basic Arrs'] = row[3]
+    record['SRA Arrs'] = row[4]
     '''
 
 
 
 
-    record['SRA Arrs'] = row[4]
+
     record['Travel'] = row[5]
     record['Dep Care'] = row[6]
     record['Mileage'] = row[7]
