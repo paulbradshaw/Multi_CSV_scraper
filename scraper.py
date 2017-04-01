c5ca1fab6d74f2a48844698bb00c3f865b19cea4
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-01 14:23:07 +0100

    copy from scraperwiki
    https://classic.scraperwiki.com/editor/raw/birminghamcouncilexpenditure

--- scraper.py
+++ scraper.py
@@ -1,24 +1,29 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import csv
+#guidance on csv library at https://scraperwiki.com/docs/python/python_csv_guide/
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+#scrape the csv file into new variable 'data'
+data = scraperwiki.scrape('http://www.birmingham.gov.uk/cs/Satellite?blobcol=urldata&blobheader=application%2Fvnd.ms-excel&blobheadername1=Content-Disposition&blobkey=id&blobtable=MungoBlobs&blobwhere=1223518937294&ssbinary=true&blobheadervalue1=attachment%3B+filename%3D300627Payments_over_%C2%A3500_October_2012.csv')
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+#use .reader function and .splitlines() method to put 'data' into csv object 'reader'
+reader = csv.reader(data.splitlines())
+print reader
+
+record = {}
+
+for row in reader:
+    record['Vendor'] = row[0]
+    record['Vendor Name'] = row[1]
+    record[' Invoice Amount '] = row[2]
+    record['Payment Date'] = row[3]
+    record['Doc Number'] = row[4]
+    record['Invoice Ref'] = row[5]
+    record['Cost Cente'] = row[6]
+    record['Directorate'] = row[7]
+    print record
+    scraperwiki.sqlite.save(['Doc Number'], record)
+
+#After first (header) row, this will generate an error with this data:
+#SqliteError: Binary strings must be utf-8 encoded
+
+#To see what happens next, go to: https://scraperwiki.com/scrapers/birminghamcouncilexpenditure_2nd/
