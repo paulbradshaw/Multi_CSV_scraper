9dfc1b3b3563db0816fee0b0715323a8f8446154
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-01 14:46:43 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,10 +15,11 @@ idno = 0
 for row in reader:
     record['Councillor'] = row[0]
     record['Basic'] = row[1]
+    record['SRA'] = row[2]
     '''
 
 
-    record['SRA'] = row[2]
+
     record['Basic Arrs'] = row[3]
     record['SRA Arrs'] = row[4]
     record['Travel'] = row[5]
