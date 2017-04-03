7219ee73640c83b9494aa3f44e7ce2acec0b6066
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-03 19:00:00 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,32 +1,31 @@
 import scraperwiki
 import csv
-#guidance on csv library at https://scraperwiki.com/docs/python/python_csv_guide/
+import lxml.html
 
-#scrape the csv file into new variable 'data'
+allcsvs = 'https://data.birmingham.gov.uk/dataset/voluntary-and-community-sector-funding-information'
+
+def scrape_and_find_csv(url):
+    html = scraperwiki.scrape(url)
+    root = lxml.html.fromstring(html)
+    #this selects all HTML containing link: <li><a>
+    csvs = root.cssselect('li a')
+    print csvs
+    for link in csvs:
+    #this prints the result of adding the base URL to the relative link grabbed
+    print baseurl+link.attrib.get('href')
+
+
 data = scraperwiki.scrape('https://data.birmingham.gov.uk/dataset/14492d37-1a77-4d46-9204-27363fc62149/resource/bacf38dd-3530-4c95-a0c3-83e21c9b2259/download/sgmsreportsvcsfreportsvcsfreports201415vcsfreport2014qtr1final.csv')
 
-#use .reader function and .splitlines() method to put 'data' into csv object 'reader'
-reader = csv.reader(data.splitlines())
-print reader
+reader = csv.DictReader(data.splitlines())
 
-record = {}
-
-idno = 0
 for row in reader:
-    record['ref'] = row[0]
-    record['organisation'] = row[1]
-    record['status'] = row[2]
-    record['start_date'] = row[3]
-    record['end_date'] = row[4]
-    record['revised_end'] = row[5]
-    record['directorate'] = row[6]
-    record['funding'] = row[7] #.decode("latin-1")
-    idno += 1
-    record['ID'] = idno
-    print record
-    scraperwiki.sqlite.save(['ID'], record)
-
-#This will generate an error with this data:
-#SqliteError: Binary strings must be utf-8 encoded
-
-#To see what happens next, go to: https://scraperwiki.com/scrapers/birminghamcouncilexpenditure_2nd/
+    for key, value in row.iteritems():
+        print key
+    row['Actual Funding Award'] = row['Actual Funding Award'].decode("latin-1")
+    row['Annual Total 2012/2013'] = row['Annual Total 2012/2013'].decode("latin-1")
+    row['Annual Total 2013/2014'] = row['Annual Total 2013/2014'].decode("latin-1")
+    row['Annual Total2014/2015'] = row['Annual Total2014/2015'].decode("latin-1")
+    row['Overall Total Paid'] = row['Overall Total Paid'].decode("latin-1")
+    print row['Ref. No.']
+    scraperwiki.sqlite.save(['Ref. No.'], row)
