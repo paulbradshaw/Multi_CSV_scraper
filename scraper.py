9c090b5044eb6ed47f703a24adfa2969cf535e69
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-04 06:46:35 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,14 +17,18 @@ def scrape_and_find_csv(url, idno):
         #this prints the result of adding the base URL to the relative link grabbed
         print link.attrib.get('href')
         if link.attrib.get('href')[-3:] == "csv":
+        	#add the partial CSV link to the base URL to get a full URL
             data = scraperwiki.scrape(baseurl+link.attrib.get('href'))
             reader = csv.DictReader(data.splitlines())
             for row in reader:
+                #incremenet id number for index
                 idno = idno+1
+                #show the keys in case we need to change any
                 for key, value in row.iteritems():
                     print key
                     print value
                 row['id'] = idno
+                #store the link attribute
                 row['url'] = link.attrib.get('href')
                 scraperwiki.sqlite.save(['id'], row)
 
